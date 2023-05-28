566db73c68762124baaac52cae29a40a8fb70af0
Nima <74507096+Nimausfi@users.noreply.github.com>
2023-05-28 14:09:38 -0700

    Initial commit

---
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.hpp"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartLocation = GetActorLocation();
+
+	FString Name = GetName();
+
+	UE_LOG(LogTemp, Display, TEXT("BeginPlay: %s"), *Name);
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	MovePlatform(DeltaTime);
+	RotatePlatform(DeltaTime);
+
+}
+
+void AMovingPlatform::MovePlatform(float DeltaTime)
+{
+	// Reverse direction of motion if gone too far
+	if (ShouldPlatformReturn())
+	{
+		FVector MoveDirection = PlatformVelocity.GetSafeNormal();
+		StartLocation = StartLocation + MoveDirection * MoveDistance;
+		SetActorLocation(StartLocation);
+		PlatformVelocity = -PlatformVelocity;
+	}
+	else {
+	// Move platform forwards
+	// Get vector to that location
+	FVector CurrentLocation = GetActorLocation();
+
+	// Add vector to that location
+	CurrentLocation = CurrentLocation + (PlatformVelocity * DeltaTime);
+
+	// Set the location
+	SetActorLocation(CurrentLocation);
+	}
+
+}
+
+void AMovingPlatform::RotatePlatform(float DeltaTime)
+{
+	AddActorLocalRotation(RotationVelocity * DeltaTime);
+}
+
+bool AMovingPlatform::ShouldPlatformReturn() const
+{
+	// Send platform back if gone too far
+	// Check how far we've moved
+	return GetDistanceMoved() > MoveDistance;
+}
+
+float AMovingPlatform::GetDistanceMoved() const 
+{	
+	// Check how far we've moved
+	return FVector::Dist(StartLocation, GetActorLocation());
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# UEOpenWorld
+## Open World Game
+
+**This project was developed using Unreal Engine 5.2 with C++ and Blueprint features.**
+<br />
  
+<img src="Images/pic01.png" width=800>
+
+In this game, the character should pass through challenging obstacles before reaching to reward. On the last ground, the character will be able to drive a sports car and experience accelerating, car jumping, and drifting.
+<br />
+
+<img src="Images/pic02.png" width=800>
+
+By going to the SportsCar_Pawn Blueprint Class, the details of the car (steering wheel, engine, brake, mass) are adjustable to customize the car.
+<br />
+
+<img src="Images/pic03.png" width=800>
+
+## Future Work
+
+Add more features to the project, some ideas are: enter and exit car animations, UIs and HUDs, more playable/drivable cars, etc. Any contributions to this project would be greatly appreciated. 
+
+## References
+
+This project was developed on top of [Unreal Engine 5 C++ Developer course](https://www.udemy.com/course/unrealcourse/).
+
+All used assets and packs are available to download from Unreal Engine Marketplace:
+<br />
+[Stylized Character Kit: Casual 01](https://www.unrealengine.com/marketplace/en-US/product/stylized-male-character-kit-casual/)
+<br />
+[Unreal Learning Kit](https://www.unrealengine.com/marketplace/en-US/product/unreal-learning-kit/)
+<br />
+[Vehicle Variety Pack](https://www.unrealengine.com/marketplace/en-US/product/bbcb90a03f844edbb20c8b89ee16ea32/)
+
